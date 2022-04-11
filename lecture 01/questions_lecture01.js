0d6385727f4b655bf3852814710b76baddf5ec0b
Auriol Degbelo <auriol.degbelo@uni-muenster.de>
2022-04-11 10:48:13 +0200

    small changes made during the presence time

---
+++ lecture 01/questions_lecture01.js
@@ -0,0 +1,45 @@
+
+// Deklaration: Name und Typ der Variable anzugeben
+
+//var a = 10 // initialisierung
+
+"use strict"
+
+var initialized = 10
+
+var not_initialized
+
+var null_referenz = null
+
+console.log(initialized)
+console.log(not_initialized)
+console.log(null_referenz)
+
+var u = 0
+var v = null
+
+console.log(typeof(u))
+console.log(typeof(v))
+
+/*
+let a
+a = 10 
+console.log(a)
+
+*/
+
+/*
+var a = 10 
+
+var a = 15 
+
+console.log(a)
+
+
+let b = 20
+
+let b = 40
+
+console.log(b)
+
+*/
