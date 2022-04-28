8d8993073e135d63f11fb7abab081d9205b933ea
Auriol Degbelo <auriol.degbelo@uni-muenster.de>
2022-04-28 13:23:38 +0200

    last changes before recording

--- lecture 04/structuring examples/Example3/main (def).js
+++ lecture 04/structuring examples/Example3/main (def).js
@@ -6,20 +6,18 @@
 function data_reading()
 {
     /* I read data, e.g. from a file, from a formular, from an api */
-    return 2
 }
 
 
-function data_processing(inputdata)
+function data_processing()
 {
 
     /* I process the data that was read and do cool stuff with it */
-    return 10 * inputdata
 }
 
-function user_interface_update(inputdata)
+function user_interface_update()
 {
 
     /* I show the results of my data processing to users */
-    document.getElementById("my_first_html_element").innerHTML= `The value of the processing is ${inputdata}`
+    document.getElementById("my_first_html_element").innerHTML= "Here are my results from the underscore scenario"
 }
--- lecture 04/structuring examples/Example3/main (exec).js
+++ lecture 04/structuring examples/Example3/main (exec).js
@@ -3,14 +3,7 @@
 window.onload = function ()
 {
     // executing the function 
-   let data = data_reading()
-   console.log(data)
-
-   let data_processed = data_processing(data)
-   console.log(data_processed)
-
-user_interface_update(data_processed)
-
-    
+   
+   user_interface_update()
 
 }
