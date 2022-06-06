0c5a3d8c3cc368c816120ada16ff350c013764a8
Auriol Degbelo <auriol.degbelo@uni-muenster.de>
2022-06-06 23:28:48 +0200

    new mongodb template

--- lecture 08/ToDelete/database_test.js
+++ lecture 08/ToDelete/database_test.js
@@ -1,5 +1,5 @@
 // template to add to and retrieve documents from a MongoDB database
-const MongoClient = require('mongodb').MongoClient
+const { MongoClient } = require('mongodb')
 const assert = require('assert')
 
 const url = 'mongodb://localhost:27017' // connection URL
@@ -10,61 +10,54 @@ const dbName = 'mydb' // database name
 
 const collectionName = 'test' // collection name
 
-// Use connect method to connect to the server
-client.connect(function(err) 
+
+
+// adapted from https://www.mongodb.com/docs/drivers/node/current/quick-start/
+async function run () 
 {
-  assert.equal(null, err)
 
-  console.log('Connected successfully to server')
+      await client.connect()
+      console.log('Connected successfully to server')
 
-  const db = client.db(dbName)
+      const db = client.db(dbName)
+
+      const collection = db.collection(collectionName)
+
+      const data = [
+        { name: 'John', address: 'Highway 71'},
+        { name: 'Peter', address: 'Lowstreet 4'},
+        { name: 'Amy', address: 'Apple st 652'},
+        { name: 'Hannah', address: 'Mountain 21'},
+        { name: 'Michael', address: 'Valley 345'},
+        { name: 'Sandy', address: 'Ocean blvd 2'},
+        { name: 'Betty', address: 'Green Grass 1'},
+        { name: 'Richard', address: 'Sky st 331'},
+        { name: 'Susan', address: 'One way 98'},
+        { name: 'Vicky', address: 'Yellow Garden 2'},
+        { name: 'Ben', address: 'Park Lane 38'},
+        { name: 'William', address: 'Central st 954'},
+        { name: 'Chuck', address: 'Main Road 989'},
+        { name: 'Viola', address: 'Sideway 1633'}
+      ]
+
+
+      // insert new documents in my collection
+     //insertDocuments(collection, data)
+
+     countRows (collection)
+
+     // find documents and close the session
+     findDocuments(collection, client)
   
-  /*
-    // create a collection 
-    db.createCollection(collectionName, function(err, res) 
-    {
-        if (err) throw err
-        console.log("Collection created!")
-    })
+}
+run()
+
+   
   
-    */
-  const collection = db.collection(collectionName)
 
-  const data = [
-    { name: 'John', address: 'Highway 71'},
-    { name: 'Peter', address: 'Lowstreet 4'},
-    { name: 'Amy', address: 'Apple st 652'},
-    { name: 'Hannah', address: 'Mountain 21'},
-    { name: 'Michael', address: 'Valley 345'},
-    { name: 'Sandy', address: 'Ocean blvd 2'},
-    { name: 'Betty', address: 'Green Grass 1'},
-    { name: 'Richard', address: 'Sky st 331'},
-    { name: 'Susan', address: 'One way 98'},
-    { name: 'Vicky', address: 'Yellow Garden 2'},
-    { name: 'Ben', address: 'Park Lane 38'},
-    { name: 'William', address: 'Central st 954'},
-    { name: 'Chuck', address: 'Main Road 989'},
-    { name: 'Viola', address: 'Sideway 1633'}
-  ]
 
-  
-  // insert new documents in my collection
-  //insertDocuments(collection, data)
+/* Definition of the different functions starting here */
 
-  findDocuments(collection)
-
-  /*
-  countRows (collection)
-
-  findDocuments(db, collection, function()
-  {
-    console.log("Closing the client...")
-    client.close()
-  })
-
-  */
-  
-})
 
 // function to insert documents in the collection
 // adapted from https://www.mongodb.com/docs/drivers/node/current/usage-examples/insertMany/
@@ -74,27 +67,22 @@ const insertDocuments = async function (collection, data)
     // this option prevents additional documents from being inserted if one fails
     const options = { ordered: true }
     const result = await collection.insertMany(data, options)
-    console.log(`${result.insertedCount} documents were inserted in the collection`);
+    console.log(`${result.insertedCount} documents were inserted in the collection`)
 
-    /*
-    // Insert some documents
-    collection.insertMany(data, function(err, result) 
-    {
-      assert.equal(err, null)
-      console.log(result)
-      console.log(result.result)
-      //console.log(result.result.ok)
-      assert.equal(1, result.result.n)
-      //console.log(result)
-      console.log(`Inserted ${result.insertedCount} documents into the collection`)
-      callback(result)
-    })
-    */
+}
+
+// count the number of rows in a collection
+// adapted from https://www.mongodb.com/docs/drivers/node/current/usage-examples/count/
+const countRows = async function(collection)
+{
+    const estimate = await collection.estimatedDocumentCount()
+    console.log(`Estimated number of documents in the current collection: ${estimate}`)
+
 }
 
 // find all documents in a collection
 // adapted from https://www.mongodb.com/docs/manual/reference/method/db.collection.find/
-const findDocuments = async function(collection) 
+const findDocuments = async function(collection, client) 
 {
    const cursor =  collection.find({})
 
@@ -120,17 +108,9 @@ const findDocuments = async function(collection)
 
     })
     */
-}
 
-// count the number of rows in a collection
-const countRows = function(collection)
-{
-    // count the total number of elements in my collection
-    collection.count(function (err, count) 
-    {
-        if (err) throw err
-    
-        console.log('Total Rows: ' + count)
-    })
+    // this is the last function in the chain of functions, so we close the client
+    await client.close()
 
 }
+
