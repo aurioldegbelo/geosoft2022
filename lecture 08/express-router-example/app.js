4c1f08eef2e855b032281d5bb5c354f17c3a1e5d
Auriol Degbelo <auriol.degbelo@uni-muenster.de>
2022-06-08 14:46:14 +0200

    express router example updated

--- lecture 08/express-router-example/app.js
+++ lecture 08/express-router-example/app.js
@@ -1,18 +1,19 @@
 // the example is slightly adapted from https://expressjs.com/en/guide/routing.html
-// The purpose of the example is to define a birds Router
+// The purpose of the example is to define a use the birds router via a custom route (called here /route_using_router)
 const express = require('express')
 const app = express()
-const birds = require('./birds')
 
+const birds = require('./birds') // load the birds router
 const port = 3000 // unsafe ports: https://superuser.com/questions/188058/which-ports-are-considered-unsafe-by-chrome 
 
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
+app.get('/', (req, res) => 
+{
   res.send('hello world')
 })
 
-app.use('/birds', birds)
+app.use('/route_using_router', birds)
 
 
 
