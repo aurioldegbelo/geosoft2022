01753756f29f61d24353b6f5bb8064a87a19ab35
Auriol Degbelo <auriol.degbelo@uni-muenster.de>
2022-06-08 11:38:42 +0200

    Update express_app_template.js

--- lecture 08/basic templates/express_app_template.js
+++ lecture 08/basic templates/express_app_template.js
@@ -1,5 +1,5 @@
 const express = require('express')
-//const path = require('path')
+const path = require('path')
 
 const app = express()
 const port = 3000 // unsafe ports: https://superuser.com/questions/188058/which-ports-are-considered-unsafe-by-chrome 
@@ -23,21 +23,22 @@ app.use(myLogger)
 
 */
 
+// define a route
 // function taken from https://expressjs.com/de/starter/hello-world.html
-// route
 app.get('/', (req, res) => 
 {
   res.send('Hello World!')
 })
 
+// serve static files
 // function taken from https://expressjs.com/de/starter/static-files.html 
-// static
 app.use(express.static('public'))
 //app.use('/static', express.static('public'))
 //app.use('/static', express.static(path.join(__dirname, 'public')))
+//console.log(__dirname) // __dirname tells you the absolute path of the directory containing the currently executing file
+//console.log(path.join(__dirname, 'public'))
 
-console.log(__dirname)
-console.log(path.join(__dirname, 'public'))
+
 
 
 app.listen(port, () => 
