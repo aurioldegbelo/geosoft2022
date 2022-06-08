1d12c59b88ad21f17f4b6aeba965c81e5459666c
Auriol Degbelo <auriol.degbelo@uni-muenster.de>
2022-06-09 00:50:18 +0200

    getting the structure ready

--- lecture 08/scenario (express + mongodb)/app.js
+++ lecture 08/scenario (express + mongodb)/app.js
@@ -4,8 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+
+var homeRouter = require('./routes/home');
+var searchRouter = require('./routes/search');
+var addRouter = require('./routes/add');
 
 var app = express();
 
@@ -19,8 +23,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+
+
+// adding my custom routes
+app.use('/', homeRouter);
+app.use('/home', homeRouter);
+app.use('/search', searchRouter);
+app.use('/add', addRouter);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
