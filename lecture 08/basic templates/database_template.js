323c4da8a25dcc14318f0b52f9b8716822bedbc8
Auriol Degbelo <auriol.degbelo@uni-muenster.de>
2022-06-08 11:33:30 +0200

    new templates created

--- lecture 08/basic templates/database_template.js
+++ lecture 08/basic templates/database_template.js
@@ -1,6 +1,5 @@
 // template to add to and retrieve documents from a MongoDB database
-const MongoClient = require('mongodb').MongoClient
-const assert = require('assert')
+const { MongoClient } = require('mongodb')
 
 const url = 'mongodb://localhost:27017' // connection URL
 
@@ -10,95 +9,101 @@ const dbName = 'mydb' // database name
 
 const collectionName = 'test' // collection name
 
-// Use connect method to connect to the server
-client.connect(function(err) 
+
+// adapted from https://www.mongodb.com/docs/drivers/node/current/quick-start/
+async function run () 
 {
-  assert.equal(null, err)
 
-  console.log('Connected successfully to server')
+      await client.connect()
+      console.log('Connected successfully to server')
 
-  const db = client.db(dbName)
-  /*
-    // create a collection 
-    db.createCollection(collectionName, function(err, res) 
-    {
-        if (err) throw err
-        console.log("Collection created!")
-    })
-  */
-  const collection = db.collection(collectionName)
+      const db = client.db(dbName)
 
-  const data = [
-    { name: 'John', address: 'Highway 71'},
-    { name: 'Peter', address: 'Lowstreet 4'},
-    { name: 'Amy', address: 'Apple st 652'},
-    { name: 'Hannah', address: 'Mountain 21'},
-    { name: 'Michael', address: 'Valley 345'},
-    { name: 'Sandy', address: 'Ocean blvd 2'},
-    { name: 'Betty', address: 'Green Grass 1'},
-    { name: 'Richard', address: 'Sky st 331'},
-    { name: 'Susan', address: 'One way 98'},
-    { name: 'Vicky', address: 'Yellow Garden 2'},
-    { name: 'Ben', address: 'Park Lane 38'},
-    { name: 'William', address: 'Central st 954'},
-    { name: 'Chuck', address: 'Main Road 989'},
-    { name: 'Viola', address: 'Sideway 1633'}
-  ]
+      const collection = db.collection(collectionName)
 
-  // insert new documents in my collection
-  insertDocuments(db, collection, data, function()
-  {
-    console.log("Insertion operation done")
-    //client.close()
-  })
+      const data = [
+        { name: 'John', address: 'Highway 71'},
+        { name: 'Peter', address: 'Lowstreet 4'},
+        { name: 'Amy', address: 'Apple st 652'},
+        { name: 'Hannah', address: 'Mountain 21'},
+        { name: 'Michael', address: 'Valley 345'},
+        { name: 'Sandy', address: 'Ocean blvd 2'},
+        { name: 'Betty', address: 'Green Grass 1'},
+        { name: 'Richard', address: 'Sky st 331'},
+        { name: 'Susan', address: 'One way 98'},
+        { name: 'Vicky', address: 'Yellow Garden 2'},
+        { name: 'Ben', address: 'Park Lane 38'},
+        { name: 'William', address: 'Central st 954'},
+        { name: 'Chuck', address: 'Main Road 989'},
+        { name: 'Viola', address: 'Sideway 1633'}
+      ]
 
-  countRows (collection)
+      // insert new documents in my collection
+     insertDocuments(collection, data)
 
-  findDocuments(db, collection, function()
-  {
-    console.log("Closing the client...")
-    client.close()
-  })
+     countRows (collection)
 
-})
-
-
-const insertDocuments = function(db, collection, data, callback) 
-{
-    // Insert some documents
-    collection.insertMany(data, function(err, result) 
-    {
-      assert.equal(err, null)
-      assert.equal(1, result.result.ok)
-      //console.log(result)
-      console.log(`Inserted ${result.insertedCount} documents into the collection`)
-      callback(result)
-    })
+     // find documents and close the session
+     findDocuments(collection, client)
+  
 }
+run()
 
-// find all documents in a collection
-const findDocuments = function(db, collection, callback) 
+
+/* Definition of the different functions starting here */
+
+// function to insert documents in the collection
+// adapted from https://www.mongodb.com/docs/drivers/node/current/usage-examples/insertMany/
+const insertDocuments = async function (collection, data) 
 {
 
-    // Find some documents
-    collection.find({}).toArray(function(err, docs) 
-    {
-      assert.equal(err, null);
-      console.log('Found the following records');
-      console.log(docs);
-      callback(docs)
-    })
+    // this option prevents additional documents from being inserted if one fails
+    const options = { ordered: true }
+    const result = await collection.insertMany(data, options)
+    console.log(`${result.insertedCount} documents were inserted in the collection`)
+
 }
 
 // count the number of rows in a collection
-const countRows = function(collection)
+// adapted from https://www.mongodb.com/docs/drivers/node/current/usage-examples/count/
+const countRows = async function(collection)
 {
-    // count the total number of elements in my collection
-    collection.count(function (err, count) 
-    {
-        if (err) throw err
-    
-        console.log('Total Rows: ' + count)
-    })
+    const estimate = await collection.estimatedDocumentCount()
+    console.log(`Estimated number of documents in the current collection: ${estimate}`)
 
 }
+
+// find all documents in a collection
+// adapted from https://www.mongodb.com/docs/manual/reference/method/db.collection.find/
+const findDocuments = async function(collection, client) 
+{
+   const cursor =  collection.find({})
+
+   const results = await cursor.toArray()
+   
+   if (results.length == 0)
+   {
+
+    console.log("No documents found!")
+
+   }
+   else
+   {
+    console.log(`Found ${results.length} documents in the collection...`);
+    console.log(results)
+
+   }
+
+    /* // function to access the documents one by one
+    await cursor.forEach((doc) =>
+    {
+        console.log(doc)
+
+    })
+    */
+
+    // this is the last function in the chain of functions, so we close the client
+    await client.close()
+
+}
+
--- lecture 08/basic templates/express_app_template.js
+++ lecture 08/basic templates/express_app_template.js
@@ -4,16 +4,40 @@ const express = require('express')
 const app = express()
 const port = 3000 // unsafe ports: https://superuser.com/questions/188058/which-ports-are-considered-unsafe-by-chrome 
 
+
+/* // Middleware
+var myLogger = function (req, res, next) 
+{
+  console.log('LOGGED')
+  next()
+}
+
+var mySecondLogger = function (req, res, next) 
+{
+  console.log('SECOND LOGGING')
+  next()
+}
+
+app.use(mySecondLogger)
+app.use(myLogger)
+
+*/
+
 // function taken from https://expressjs.com/de/starter/hello-world.html
+// route
 app.get('/', (req, res) => 
 {
   res.send('Hello World!')
 })
 
 // function taken from https://expressjs.com/de/starter/static-files.html 
+// static
 app.use(express.static('public'))
 //app.use('/static', express.static('public'))
 //app.use('/static', express.static(path.join(__dirname, 'public')))
+
+console.log(__dirname)
+console.log(path.join(__dirname, 'public'))
 
 
 app.listen(port, () => 
