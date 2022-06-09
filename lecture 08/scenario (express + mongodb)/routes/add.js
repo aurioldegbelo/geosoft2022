10bdf231b5ee8b261e8b62bbb32f4f02950b1ca3
Auriol Degbelo <auriol.degbelo@uni-muenster.de>
2022-06-09 17:04:40 +0200

    add notification pug

--- lecture 08/scenario (express + mongodb)/routes/add.js
+++ lecture 08/scenario (express + mongodb)/routes/add.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('add', { title: 'Addition Page' });
 });
 
+
+router.post('/newpoi', function(req, res, next) 
+{
+  console.log("A new poi has been added")
+
+  console.log(req.body)
+  let poi = {}
+  poi.poiname = req.body.pname
+  poi.cityname = req.body.cname
+  poi.coordinates = req.body.longlat
+  poi.link = req.body.picurl
+
+})
+
+
 module.exports = router;
